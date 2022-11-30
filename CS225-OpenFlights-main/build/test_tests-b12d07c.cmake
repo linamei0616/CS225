35b3ee90d85de06985e632e08b55808725a3d94b
Lina Mei <lmei25@illinois.edu>
2022-11-30 23:23:11 +0000

    Finished BFS, no test cases yet

---
+++ CS225-OpenFlights-main/build/test_tests-b12d07c.cmake
@@ -0,0 +1,3 @@
+add_test( [==[testing test case]==] /workspaces/CS225/CS225-OpenFlights-main/build/test [==[testing test case]==]  )
+set_tests_properties( [==[testing test case]==] PROPERTIES WORKING_DIRECTORY /workspaces/CS225/CS225-OpenFlights-main/build)
+set( test_TESTS [==[testing test case]==])
